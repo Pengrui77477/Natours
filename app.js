b6226a627f8cdf2cfacd311cc604d646231cb7c2
Ray <774772411@qq.com>
2022-11-12 19:27:19 +0800

    fix

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toLocaleString();
