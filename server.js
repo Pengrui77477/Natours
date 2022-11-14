478079cf7a390a05b0291f7f95cf9b36fb59a217
Ray <774772411@qq.com>
2022-11-14 17:39:14 +0800

    connect database

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
 const app = require('./app');
 
+
+mongoose.connect(process.env.DATABASE_LOCAL,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+}).then(con=>console.log('数据库连接成功'))
+.catch(err => console.log(err))
 
 const port = process.env.PORT;
 app.listen(port, () => {
