c392aa56f8da08d199ad0c8ba98de739d09ead00
Ray <774772411@qq.com>
2022-11-14 17:22:26 +0800

    add env

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
-// app.use(express.static(`${__dirname}/public`));
+
 app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
